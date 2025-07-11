e9f8b3a8a396fdc050258d9ab6685c7f923734b5
gaurav <gauravfate393@gmail.com>
2025-07-11 12:52:20 +0530

    frontend old deleted

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -2,6 +2,35 @@ import React from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
+import ProductGrid from "../components/Products/ProductGrid";
+
+const placeholderProducts = [
+    {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        image: [{ url: "https://picsum.photos/500/500?random=5" }],
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        image: [{ url: "https://picsum.photos/500/500?random=3" }],
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        image: [{ url: "https://picsum.photos/500/500?random=1" }],
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        image: [{ url: "https://picsum.photos/500/500?random=6" }],
+    },
+];
 
 const Home = () => {
     return (
@@ -9,6 +38,17 @@ const Home = () => {
             <Hero />
             <GenderCollectionSection />
             <NewArrivals />
+
+            {/* Best Seller */}
+            <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
+            <ProductDetails />
+
+            <div className="container mx-auto">
+                <h2 className="text-3xl text-center font-bold mb-4">
+                    Top Wear for Women
+                </h2>
+                <ProductGrid products={placeholderProducts} />
+            </div>
         </div>
     );
 };
--- frontend/src/common/role.js
+++
@@ -1,4 +0,0 @@
-export const role = {
-    ADMIN : "ADMIN",
-    GENERAL: "GENERAL"
-}
