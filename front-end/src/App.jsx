dbc38d159edcde645ef7dc33ae5ed0e58dc8a46a
gaurav <gauravfate393@gmail.com>
2025-07-25 19:26:30 +0530

    Login and register add

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,15 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./Pages/Home";
 import { Toaster } from "sonner";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
 
 function App() {
     return (
         <BrowserRouter>
             <Toaster position="top-right" />
+
             <Routes>
                 <Route path="/" element={<UserLayout />}>
                     {/* User Layout */}
                     <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="profile" element={<Profile /> } />
                 </Route>
 
                 <Route>{/* Admin Layout */}</Route>
